d4c0b594fd27138afc52c733aa3b1f881c88bc2d
orangelily <547207352@qq.com>
2016-03-19 10:56:43 +0800

    smart 初步提交

---
+++ Smart/Smart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Smart
+//
+//  Created by orange on 16/3/19.
+//  Copyright © 2016年 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Smart/Smart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Smart
+//
+//  Created by orange on 16/3/19.
+//  Copyright © 2016年 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
